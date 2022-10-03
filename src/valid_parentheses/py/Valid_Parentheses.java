4198609724fa1e3a2fd21124fd235cc338451745
Vinayak Hene <Vinayak Hene@DESKTOP-87HE5SR>
2022-10-03 16:44:06 +0530

    [vikram hene] :- Solution Valid parentheses

---
+++ src/valid_parentheses/py/Valid_Parentheses.java
@@ -0,0 +1,48 @@
+package valid_parentheses.py;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+	
+	
+	public static boolean isValid(String s) {
+		
+
+		 Stack<Character> st1 = new Stack<>();
+	     
+	     for (char x : s.toCharArray()) {
+	       
+	     if (x == '(' && x == '{' && x == '[') {
+	    	 st1.push(x);
+	         }
+	     
+	         else if (x == ')' && !st1.isEmpty() && st1.peek() == '(') 
+	         {
+	        	 st1.pop();
+	         } 
+	         else if (x == '}' && !st1.isEmpty() && st1.peek() == '{') 
+	         {
+	        	 st1.pop();
+	         } 
+	         else if (x == ']' && !st1.isEmpty() && st1.peek() == '[') 
+	         {
+	        	 st1.pop();
+	         }
+	      
+	         else {
+	             return false;
+	         }
+	     }
+	     return st1.isEmpty(); 
+	 }
+	public static void main(String[] args) {
+		
+		System.out.println(isValid("()"));
+		System.out.println(isValid("()[]{}"));
+		System.out.println(isValid("(]"));
+		System.out.println(isValid("([)]"));
+		System.out.println(isValid("{[]}"));
+	}
+	
+
+}
